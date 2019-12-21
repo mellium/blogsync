05fe161eb2a72025d1ce2e38050ada5c824d3ab9
Sam Whited <sam@samwhited.com>
2019-12-21 18:01:31 -0500

    internal/blog: new package for handling posts

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module mellium.im/blogsync
 go 1.13
 
 require (
+	github.com/BurntSushi/toml v0.3.1
 	github.com/writeas/go-writeas/v2 v2.0.2
+	gopkg.in/yaml.v2 v2.2.7
 	mellium.im/cli v0.1.0
 )
---
+++ internal/blog/blog.go
@@ -0,0 +1,154 @@
+// Copyright 2019 The Blog Sync Contributors.
+// Use of this source code is governed by the BSD 2-clause
+// license that can be found in the LICENSE file.
+
+// Package blog contains helpers for manipulating site and blog source files.
+package blog
+
+import (
+	"bufio"
+	"bytes"
+	"io"
+	"os"
+	"path/filepath"
+	"strconv"
+	"time"
+
+	"github.com/BurntSushi/toml"
+	"gopkg.in/yaml.v2"
+)
+
+// The header of supported metadata types.
+const (
+	HeaderTOML = "+++\n"
+	HeaderYAML = "---\n"
+)
+
+// WalkPages walks the file tree rooted at root and calls walkFn for each page.
+// It skips any files that do not end in the extension ".markdown" or ".md".
+func WalkPages(root string, walkFn filepath.WalkFunc) error {
+	return filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		ext := filepath.Ext(path)
+		switch {
+		case ext != ".md" && ext != ".markdown":
+			// Skip non-markdown files
+			return nil
+		case info.IsDir():
+			// Decend into directories, but don't call walkFn for them.
+			return nil
+		}
+
+		return walkFn(path, info, err)
+	})
+}
+
+// Metadata contains parsed metadata including the type of the metadata in the
+// file, and the offset of where the metadata ends.
+type Metadata struct {
+	header string
+	meta   map[string]interface{}
+}
+
+// Decode extracts metadata from the provided page.
+// It assumes the first byte is the metadata header.
+//
+// It supports decoding TOML wrapped in "+++\n" and YAML wrapped in "---\n"
+// similar to Hugo or Jekyll.
+func (m *Metadata) Decode(f io.Reader) error {
+	r := bufio.NewReader(f)
+
+	var err error
+	m.header, err = r.ReadString('\n')
+	if err != nil {
+		return err
+	}
+
+	metaBuf := new(bytes.Buffer)
+	line, err := r.ReadString('\n')
+	if err != nil && err != io.EOF {
+		return err
+	}
+	for line != m.header {
+		_, err := metaBuf.WriteString(line)
+		if err != nil {
+			return err
+		}
+
+		line, err = r.ReadString('\n')
+		if err != nil && err != io.EOF {
+			return err
+		}
+	}
+
+	m.meta = make(map[string]interface{})
+	switch m.header {
+	case HeaderTOML:
+		err = toml.Unmarshal(metaBuf.Bytes(), &m.meta)
+	case HeaderYAML:
+		err = yaml.Unmarshal(metaBuf.Bytes(), m.meta)
+	}
+	return err
+}
+
+// Get returns the value parsed for the given key, and whether or not the key
+// actually exited in the metadata.
+func (m *Metadata) Get(key string) (interface{}, bool) {
+	val, ok := m.meta[key]
+	return val, ok
+}
+
+// GetString parses the metadata value for key and returns it as a string.
+// If the underlying value is not a valid string, an empty string is returned.
+func (m *Metadata) GetString(key string) string {
+	val, ok := m.Get(key)
+	if !ok {
+		return ""
+	}
+
+	ret, _ := val.(string)
+	return ret
+}
+
+// GetBool parses the metadata value for key and returns it as a bool.
+// If the underlying value is a string GetBool attempts to parse it using
+// strconv.ParseBool.
+func (m *Metadata) GetBool(key string) bool {
+	val, ok := m.Get(key)
+	if !ok {
+		return false
+	}
+
+	switch v := val.(type) {
+	case bool:
+		return v
+	case string:
+		parsed, _ := strconv.ParseBool(v)
+		return parsed
+	}
+	return false
+}
+
+var fmts = []string{time.RFC3339, time.RFC3339Nano, "2006-01-02T15:04:05", "2006-01-02"}
+
+// GetTime parses the metadata value for key and returns it as a timestamp.
+// If the underlying value is not already a time.Time, or a string that can be
+// parsed into a valid time, a nil value is returned.
+func (m *Metadata) GetTime(key string) *time.Time {
+	val, ok := m.Get(key)
+	if !ok {
+		return nil
+	}
+
+	switch t := val.(type) {
+	case time.Time:
+		return &t
+	case string:
+		for _, timeFmt := range fmts {
+			out, err := time.Parse(timeFmt, t)
+			if err == nil {
+				return &out
+			}
+		}
+	}
+	return nil
+}
