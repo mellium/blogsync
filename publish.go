f28d35db4756c0cc73215e136f04303f71185dfe
Sam Whited <sam@samwhited.com>
2019-12-21 23:22:45 -0500

    blogsync: support parsing templates for the body

--- publish.go
+++ publish.go
@@ -12,6 +12,8 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
+	"text/template"
 
 	"github.com/writeas/go-writeas/v2"
 	"mellium.im/blogsync/internal/blog"
@@ -22,10 +24,12 @@ func publishCmd(client *writeas.Client, logger, debug *log.Logger) *cli.Command 
 	var (
 		collection string
 		content    = "content/"
+		tmpl       = "{{.Body}}"
 	)
 	flags := flag.NewFlagSet("publish", flag.ContinueOnError)
 	flags.StringVar(&collection, "collection", collection, "The default collection for posts that don't include `collection' in their frontmatter")
 	flags.StringVar(&content, "content", content, "A directory containing pages and posts")
+	flags.StringVar(&tmpl, "tmpl", tmpl, "A template using Go's html/template format, to load from a file use @filename")
 
 	return &cli.Command{
 		Usage: "publish [options]",
@@ -34,6 +38,23 @@ func publishCmd(client *writeas.Client, logger, debug *log.Logger) *cli.Command 
 Expects an API token to be exported as $%s.`, envToken),
 		Flags: flags,
 		Run: func(cmd *cli.Command, args ...string) error {
+			compiledTmpl := template.New("root")
+			var err error
+			if tmplFile := strings.TrimPrefix(tmpl, "@"); tmpl != tmplFile {
+				// If the template argument starts with "@" it is a filename that we
+				// should load.
+				compiledTmpl, err = template.ParseFiles(tmplFile)
+				if err != nil {
+					return fmt.Errorf("error compiling template file %s: %v", tmplFile, err)
+				}
+			} else {
+				// Otherwise, it is a raw template and we should compile it.
+				compiledTmpl, err = compiledTmpl.Parse(tmpl)
+				if err != nil {
+					return fmt.Errorf("error compiling template: %v", err)
+				}
+			}
+
 			return blog.WalkPages("content/", func(path string, info os.FileInfo, err error) error {
 				debug.Printf("opening %s", path)
 				fd, err := os.Open(path)
@@ -78,16 +99,28 @@ Expects an API token to be exported as $%s.`, envToken),
 					return nil
 				}
 				body = bytes.TrimSpace(body)
-				if len(body) == 0 {
+
+				var bodyBuf strings.Builder
+				err = compiledTmpl.Execute(&bodyBuf, struct {
+					Body string
+					Meta *blog.Metadata
+				}{
+					Body: string(body),
+					Meta: meta,
+				})
+				if err != nil {
+					logger.Printf("error executing template for file %s: %v", path, err)
+					return nil
+				}
+				if bodyBuf.Len() == 0 {
 					// Apparently write.as doesn't like posts that don't have a body.
 					logger.Printf("post %s has no body, skipping", path)
 					return nil
 				}
-
 				_, err = client.CreatePost(&writeas.PostParams{
 					Created:    created,
 					Title:      title,
-					Content:    string(body),
+					Content:    bodyBuf.String(),
 					Collection: collection,
 
 					// TODO: Font string, get from metadata or config
